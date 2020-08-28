edce22841cd1739adf5fbdf4f828a203086091c4
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2020-08-29 02:25:52 +0430

    added sending valid req for signup

--- client/src/pages/Signup/InputBox.js
+++ client/src/pages/Signup/InputBox.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const InputBox = ({ type, name, placeholder, icon }) => {
-	return (
-		<div className='input-box' onClick={divertFocusToInput}>
-			<div className='input-box__icon'>{icon}</div>
-			<input type={type} name={name} placeholder={placeholder} />
-		</div>
-	);
+const InputBox = ({ type, name, placeholder, icon, changeHandler }) => {
+  return (
+    <div className="input-box" onClick={divertFocusToInput}>
+      <div className="input-box__icon">{icon}</div>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        onChange={(e) => changeHandler(e)}
+      />
+    </div>
+  );
 };
 
 const divertFocusToInput = (e) => {
-	e.currentTarget.getElementsByTagName('input')[0].focus();
+  e.currentTarget.getElementsByTagName("input")[0].focus();
 };
 
 export default InputBox;
--- client/src/pages/Signup/Left.js
+++ client/src/pages/Signup/Left.js
@@ -1,20 +1,89 @@
-import React from 'react';
-import InputBox from './InputBox';
+import React, { useState } from "react";
+import InputBox from "./InputBox";
+import axios from "axios";
 
-import PersonIcon from '@material-ui/icons/Person';
-import EmailIcon from '@material-ui/icons/Email';
-import LockIcon from '@material-ui/icons/Lock';
+import PersonIcon from "@material-ui/icons/Person";
+import EmailIcon from "@material-ui/icons/Email";
+import LockIcon from "@material-ui/icons/Lock";
 
 const Left = (props) => {
-	return (
-		<div className='left'>
-			<div className='left__title'>Join Us Now !</div>
-			<InputBox type='text' name='username' placeholder='Username' icon={<PersonIcon />} />
-			<InputBox type='email' name='email' placeholder='Email' icon={<EmailIcon />} />
-			<InputBox type='password' name='password' placeholder='Password' icon={<LockIcon />} />
-			<div className='button button--primary'>SIGN UP</div>
-		</div>
-	);
+  const [userInfo, setUserInfo] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  console.log(userInfo);
+
+  const changeHandler = (e) => {
+    const name = e.currentTarget.name;
+    const value = e.currentTarget.value;
+    if (name === "username") {
+      setUserInfo((currentUserInfo) => ({
+        ...currentUserInfo,
+        userName: value,
+      }));
+    }
+    if (name === "email") {
+      setUserInfo((currentUserInfo) => ({
+        ...currentUserInfo,
+        email: value,
+      }));
+    }
+    if (name === "password") {
+      setUserInfo((currentUserInfo) => ({
+        ...currentUserInfo,
+        password: value,
+      }));
+    }
+  };
+
+  const signupHandler = async () => {
+    const result = await axios.post("http://localhost:4000/user-auth/sing-up", {
+      userName: userInfo.userName,
+      password: userInfo.password,
+      email: userInfo.email,
+    });
+
+    if (result.data.error) {
+      alert(result.data.error);
+    } else {
+      alert(result.data.message);
+    }
+
+    console.log(result);
+  };
+
+  return (
+    <div className="left">
+      <div className="left__title">Join Us Now !</div>
+      <InputBox
+        type="text"
+        name="username"
+        placeholder="Username"
+        icon={<PersonIcon />}
+        changeHandler={changeHandler}
+      />
+      <InputBox
+        type="email"
+        name="email"
+        placeholder="Email"
+        icon={<EmailIcon />}
+        changeHandler={changeHandler}
+      />
+      <InputBox
+        type="password"
+        name="password"
+        placeholder="Password"
+        icon={<LockIcon />}
+        changeHandler={changeHandler}
+      />
+
+      <div className="button button--primary" onClick={signupHandler}>
+        SIGN UP
+      </div>
+    </div>
+  );
 };
 
 export default Left;
