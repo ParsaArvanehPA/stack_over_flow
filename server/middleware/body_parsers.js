1b720bcf7d539460309c14b62ff221f0012591a6
BijanEisapour <bijanprogrammer@gmail.com>
2020-08-30 10:03:30 +0430

    Fixed an issue with 'cors'

--- server/middleware/body_parsers.js
+++ server/middleware/body_parsers.js
@@ -1,26 +1,27 @@
-const { Router } = require("express");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const { Router } = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 //////////////////////
 
 const router = Router();
 
-router.use(express.json({ limit: "50mb" }));
+router.use(express.json({ limit: '50mb' }));
 
 router.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
+	cors({
+		origin      : 'http://localhost:3000',
+		credentials : true
+	})
 );
 
 router.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-    parameterLimit: 50000,
-  })
+	bodyParser.urlencoded({
+		limit          : '50mb',
+		extended       : true,
+		parameterLimit : 50000
+	})
 );
 
 router.use(cookieParser());
